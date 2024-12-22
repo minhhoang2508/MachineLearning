95a5d57c80ce5a2fceef42bcb3234cfb3bf72e19
ndkienhd2004 <kienhd2004@gmail.com>
2024-12-22 21:32:22 +0700

    update main.py

--- main.py
+++ main.py
@@ -461,3 +461,26 @@ def TrainMLWithVisualization(model_class, test_data):
     oof_non_rounded = np.zeros(len(y), dtype=float) 
     oof_rounded = np.zeros(len(y), dtype=int) 
     test_preds = np.zeros((len(test_data), n_splits))
+    # Huấn luyện mô hình trên từng fold
+    for fold, (train_idx, test_idx) in enumerate(tqdm(SKF.split(X, y), desc="Training Folds", total=n_splits)):
+        X_train, X_val = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_val = y.iloc[train_idx], y.iloc[test_idx]
+       
+        model = clone(model_class)
+        model.fit(X_train, y_train)
+        y_train_pred = model.predict(X_train)
+        y_val_pred = model.predict(X_val)
+
+        oof_non_rounded[test_idx] = y_val_pred
+        y_val_pred_rounded = y_val_pred.round(0).astype(int)
+        oof_rounded[test_idx] = y_val_pred_rounded
+
+        train_kappa = quadratic_weighted_kappa(y_train, y_train_pred.round(0).astype(int))
+        val_kappa = quadratic_weighted_kappa(y_val, y_val_pred_rounded)
+
+        train_S.append(train_kappa)
+        test_S.append(val_kappa)
+        
+        test_preds[:, fold] = model.predict(test_data)
+        
+        print(f"Fold {fold+1} - Train QWK: {train_kappa:.4f}, Validation QWK: {val_kappa:.4f}")
